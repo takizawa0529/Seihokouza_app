948c60827b46ff08c3f4560859f90ce2f398012a
takizawa0529 <tkzw.0529@gmail.com>
2024-08-18 16:07:09 +0900

    Second Commit

--- pages/Souron.py
+++ pages/Souron.py
@@ -32,43 +32,61 @@ exam_files = {
 file_path = f'./Problems/{subject_name}/' + exam_files[subject]
 df = pd.read_csv(file_path, encoding='shift-jis')
 
-st.markdown('**生保標準生命表2018(死亡保険用)(女)**')
-image = Image.open(f'./picture/{subject_name}/{subject}/生命表.png')
-st.image(image)#, width=200)
+# 問題範囲を各ページに割り当てる
+page_to_questions = {
+    3: range(1, 6),
+    4: range(6, 11),
+    5: range(11, 16),
+    6: range(16, 21),
+    7: range(21, 23),
+    8: range(23, 25),
+    9: range(25, 27),
+    10: range(27, 29),
+    11: range(29, 31),
+    12: range(31, 35),
+    13: range(35, 39),
+    14: range(39, 42),
+    15: range(42, 44),
+    16: range(44, 46),
+    17: range(46, 48),
+    18: range(48, 50),
+    19: range(50, 51),
+}
 
-st.markdown('**生保標準生命表2018(死亡保険用)(女)**')
-image = Image.open(f'./picture/{subject_name}/{subject}/複利現価表.png')
-st.image(image)#, width=200)
-
-# 問題の表示とユーザーの回答入力
-num_questions = min(len(df), 50)
 today = dt.datetime.now()
 user_answers = []
 
-for i in range(num_questions):
-    # 問題を表示
-    st.markdown(f"**問{i+1}**")
-    st.text(df.loc[i, '問題'])
+for page, questions in page_to_questions.items():
+    # 画像を表示
+    image_path = f"./Problems/{subject_name}/{subject}/page_{page}.png"
 
-    # CSVファイルの「語群」列から選択肢を取得
-    options = df.loc[i, '語群'].split('、')  # 語群をリストに変換（CSVで「、」区切りの場合）
-    # ユーザーの回答を選択式で入力
+    if os.path.exists(image_path):
+        image = Image.open(image_path)
+        st.image(image)
+    else:
+        st.text(f"画像が見つかりませんでした: page_{page}.png")
 
-    user_answer = st.radio(label=f'あなたの回答を選んでください - 問{i+1}',
-                      options=options,
-                      horizontal=True
-                      )
-    
-    user_answers.append(user_answer)
-
-    
+    # 各ページに対応する問題を表示
+    for i in questions:
+        # 問題を表示
+        st.text('-'*100)
+        st.markdown(f"**問{i}**")
+        
+        # 選択肢を表示
+        options = df.loc[i-1, '語群'].split('、')  # 語群をリストに変換
+        user_answer = st.radio(
+            label=f'あなたの回答を選んでください - 問{i}',
+            options=options,
+            horizontal=True
+        )
+        user_answers.append(user_answer)
 
 # 答え合わせボタンをページの一番下に配置
 result_button = st.button('答え合わせ')
 
 # 「答え合わせ」ボタンの処理
 if result_button:
-    for i in range(num_questions):
+    for i in range(len(user_answers)):
         st.markdown(f"**問{i+1}**")
         st.markdown(f"**解答: {df.loc[i, '解答']}**")
 
